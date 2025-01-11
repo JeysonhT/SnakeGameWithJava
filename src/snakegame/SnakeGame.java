25c0e4c8538ec5f0e6be39d7dc85d527a24a9855
Jeysonh <jasontellezaguilar@gmail.com>
2025-01-10 23:29:11 -0600

    Integracion del icono de la ventana

--- src/snakegame/SnakeGame.java
+++ src/snakegame/SnakeGame.java
@@ -5,7 +5,12 @@
 package snakegame;
 
 import View.ventana;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.WindowConstants;
 
 /**
@@ -16,17 +21,26 @@ public class SnakeGame {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         try {
             ventana v = new ventana(480, 360);
+            File file = new File("src/resources/serpiente.png");
+            BufferedImage icon = ImageIO.read(file);
+            
+            ImageIcon image = new ImageIcon(icon);
             v.setVisible(true);
             v.setLocationRelativeTo(null);
             v.setResizable(false);
+            v.setTitle("Snake Game con Java");
+
+            v.setIconImage(image.getImage());
+
             v.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         } catch (FileNotFoundException e) {
             System.out.println(e.getMessage());
         }
     }
-    
+
 }
